dfb0c37703e3024afe8ff06b432225f29712341a
Stephen Em <stphnem@gmail.com>
2016-04-27 10:17:31 -0700

    Loop until no more pages exist

--- LNAutomator.py
+++ LNAutomator.py
@@ -24,6 +24,11 @@ class LNAutomator:
         finally:
             print 'Done searching for', text
 
+    def switch_to_main_frame(self):
+        element = WebDriverWait(self.driver, 20).until(
+                EC.frame_to_be_available_and_switch_to_it( (By.ID, "mainFrame") )
+            )
+
     def switch_to_results_frame(self):
         contentFrame = self.driver.find_element_by_css_selector("frame[title='Results Content Frame']")
         self.contentFrameName = contentFrame.get_attribute('name')
@@ -42,21 +47,41 @@ class LNAutomator:
         )
 
     def check_all_results(self):
+        print "Clicking checkbox"
         checkbox = self.driver.find_element_by_id("frm_control_box")
         checkbox.click()
 
     def click_download_doc(self):
+        print "Clicking floppy disk download"
         downloadBtn = self.driver.find_element_by_css_selector('img[alt="Download Documents"]')
         downloadBtn.click()
 
     def store_parent_window(self):
         self.parentWindow = self.driver.current_window_handle
 
+    def select_parent_window(self):
+        print "Switching to parent window", self.parentWindow
+        self.driver.switch_to.window(self.parentWindow);
+
     def switch_to_popup(self):
         handles = self.driver.window_handles;
         for handle in handles:
             self.subWindowHandler = handle
+
+        print "Switching to popup", self.subWindowHandler
         self.driver.switch_to.window(self.subWindowHandler)
+
+    def switch_to_content_from_popup(self):
+        print "Switchin to main frame"
+        element = WebDriverWait(self.driver, 20).until(
+                EC.frame_to_be_available_and_switch_to_it( (By.ID, "mainFrame") )
+        )
+        element = WebDriverWait(self.driver, 20).until(
+            EC.frame_to_be_available_and_switch_to_it( (By.NAME, self.contentFrameName) )
+        )
+
+    def next_page_exists(self):
+        return self.driver.find_elements(By.CSS_SELECTOR, 'img[alt="View next document"]') > 0;
 
     def select_text_download(self):
         mySelect = Select(self.driver.find_element_by_css_selector('#delFmt'))
@@ -77,6 +102,10 @@ class LNAutomator:
         closeBtn = self.driver.find_element_by_css_selector('img[alt="Close Window"]')
         closeBtn.click()
 
+    def click_next_page(self):
+        nextPage = self.driver.find_element(By.CSS_SELECTOR, 'img[alt="View next document"]')
+        nextPage.click();
+
 if __name__ == "__main__":
     LNAutomator = LNAutomator()
     LNAutomator.search("gun control")
@@ -90,3 +119,24 @@ if __name__ == "__main__":
     LNAutomator.click_download()
     LNAutomator.click_download_link()
     LNAutomator.close_window()
+    LNAutomator.select_parent_window()
+    LNAutomator.switch_to_content_from_popup()
+    LNAutomator.check_all_results()
+    LNAutomator.switch_to_nav_frame()
+    while (LNAutomator.next_page_exists()):
+        LNAutomator.click_next_page()
+        LNAutomator.switch_to_main_frame()
+        LNAutomator.switch_to_results_frame()
+        LNAutomator.check_all_results()
+        LNAutomator.switch_to_nav_frame()
+        LNAutomator.click_download_doc()
+        LNAutomator.store_parent_window()
+        LNAutomator.switch_to_popup()
+        LNAutomator.select_text_download()
+        LNAutomator.click_download()
+        LNAutomator.click_download_link()
+        LNAutomator.close_window()
+        LNAutomator.select_parent_window()
+        LNAutomator.switch_to_content_from_popup()
+        LNAutomator.check_all_results()
+        LNAutomator.switch_to_nav_frame()
